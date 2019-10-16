864e98d4b469dedb10f0650c0b721f2181c3fd59
javascriptninja815 <xandokan815@protonmail.com>
2019-10-16 12:40:43 +0800

    10/16 updated

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-var portsArray = [];
 var data = 'P3B';
+var conId = '';
 
 // Convert string to ArrayBuffer
-var convertStringToArrayBuffer = function(data) {
+var convertStringToArrayBuffer = function (data) {
   var buf = new ArrayBuffer(data.length);
   var bufView = new Uint8Array(buf);
   for (var i = 0; i < data.length; i++) {
@@ -17,16 +17,20 @@ var onConnect = function (connectionInfo) {
   } else if (connectionInfo.connectionId < 0) {
     throw 'Invalid Connection'
   } else {
-    chrome.serial.send(connectionInfo.connectionId, convertStringToArrayBuffer(data), function () {})
+    conId = connectionInfo.connectionId;
+    console.log('conId', conId);
+    // chrome.serial.send(connectionInfo.connectionId, convertStringToArrayBuffer(data), function () {})
+    // console.log('successfully sent' + data + ' to portsArray[0]');
   }
 }
 
 // Find Available Ports
 var onGetDevices = function (ports) {
+  var portsArray = [];
   for (var i = 0; i < ports.length; i++) {
     portsArray.push(ports[i].path);
   }
-  console.log(portsArray[0]);
+  console.log('detected ports: ', portsArray);
   if (portsArray) {
     chrome.serial.connect(portsArray[0], {
       bitrate: 115200
@@ -37,20 +41,11 @@ var onGetDevices = function (ports) {
 // Connect COM Port
 chrome.serial.getDevices(onGetDevices);
 
-
-// Send Data To Specific Port
-// var data = 'P3B';
-// chrome.serial.send(connectionId, data, callback);
-
-// const DEVICE_PATH = 'COM1';
-// var data = 'P3B';
-
-
-
-// var connection = new SerialConnection();
-
-// connection.onConnect.addListener(function() {
-//   log('connected to: ' + DEVICE_PATH);
-//   connection.send(data);
-// });
-// connection.connect(DEVICE_PATH);
+chrome.runtime.onConnectExternal.addListener(function (externalPort) {
+  externalPort.onMessage.addListener(function (msg) {
+    if (msg.type == 'from extension' && msg.type) {
+      console.log(msg.data);
+      chrome.serial.send(conId, convertStringToArrayBuffer(msg.data), function() {})
+    }
+  });
+});
